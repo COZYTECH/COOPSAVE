9ee37ab4d55a500584ef3a2533ae4fff30fcb8c3
COZYTECH <109769074+COZYTECH@users.noreply.github.com>
2026-07-03 00:59:28 +0100

    web hook

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -1,21 +1,23 @@
-const nombaWebhookIngestionService = require('../services/nombaWebhookIngestionService');
-const asyncHandler = require('../utils/asyncHandler');
+const nombaWebhookIngestionService = require("../services/nombaWebhookIngestionService");
+const asyncHandler = require("../utils/asyncHandler");
 
 const handleNombaWebhook = asyncHandler(async (req, res) => {
+  console.log("HEADERS");
+  console.log(req.headers);
   const signature = nombaWebhookIngestionService.getNombaSignature(req.headers);
 
   const result = await nombaWebhookIngestionService.ingestNombaWebhook({
     rawPayload: req.body,
-    signature
+    signature,
   });
 
   return res.status(200).json({
     success: true,
-    message: 'Webhook received.',
-    data: result
+    message: "Webhook received.",
+    data: result,
   });
 });
 
 module.exports = {
-  handleNombaWebhook
+  handleNombaWebhook,
 };
